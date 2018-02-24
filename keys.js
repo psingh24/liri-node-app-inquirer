98976a2342774ba057422ce49c78b8cfa3d500ab
Prabhdeep <ps591990@gmail.com>
2018-02-23 19:29:05 -0600

    remove old keys

--- keys.js
+++ keys.js
@@ -1,15 +1,15 @@
 
 module.exports = {
     twitKeys: {
-        consumer_key:         'iWEYGk3CkbfbpZEIVdBFsFrCq',
-        consumer_secret:      'zSWa9RWi0Ccj2R3US1fmt8iEmF81ZBNFoYfuitjh0pcBw1MU8c',
-        access_token:         '875535811260944384-XB5VPgP0O2gpwydGjf5yPftoXLjPQkH',
-        access_token_key:     '875535811260944384-XB5VPgP0O2gpwydGjf5yPftoXLjPQkH',
-        access_token_secret:  'EBy0SloYooZE3GWsZja3sZOxwOpKPVeTw17UajVq2hGNh',
+        consumer_key:         'ENTER KEY HERE',
+        consumer_secret:      'ENTER KEY HERE',
+        access_token:         'ENTER KEY HERE',
+        access_token_key:     'ENTER KEY HERE',
+        access_token_secret:  'ENTER KEY HERE',
     },
     spotifyKeys: {
-        id: "83181921eed2490091eaa49fd8785f48",
-        secret: "b94ffd4bb7c04ac1acaeb4a8f7869a18"
+        id: "ENTER KEY HERE",
+        secret: "ENTER KEY HERE"
     }
 
 }
